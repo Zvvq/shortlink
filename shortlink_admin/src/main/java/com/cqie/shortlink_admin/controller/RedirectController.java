3f3f5c94efc16eadf10988635914a43fe2094001
Jack kupol <15877767+jack-kupol@user.noreply.gitee.com>
2026-04-17 13:51:26 +0800

    feat(controller): 添加测试接口以验证控制器可用性

--- shortlink_admin/src/main/java/com/cqie/shortlink_admin/controller/RedirectController.java
+++ shortlink_admin/src/main/java/com/cqie/shortlink_admin/controller/RedirectController.java
@@ -8,6 +8,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequiredArgsConstructor
@@ -19,5 +21,11 @@ public class RedirectController {
     @GetMapping("/{shortUrl}")
     public void redirect(@PathVariable String shortUrl, HttpServletRequest request, HttpServletResponse response) {
         redirectService.redirect(shortUrl, request, response);
+    }
+
+    @ResponseBody
+    @GetMapping("/test")
+    public String test() {
+        return "test";
     }
 }
