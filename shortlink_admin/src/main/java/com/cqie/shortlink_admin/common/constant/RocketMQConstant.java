0199da3d0665a4e8d51df283998b88a339ed86af
Jack kupol <15877767+jack-kupol@user.noreply.gitee.com>
2026-04-17 15:32:24 +0800

    feat(constants): 添加短链接访问统计相关常量

--- shortlink_admin/src/main/java/com/cqie/shortlink_admin/common/constant/RocketMQConstant.java
+++ shortlink_admin/src/main/java/com/cqie/shortlink_admin/common/constant/RocketMQConstant.java
@@ -2,4 +2,8 @@ package com.cqie.shortlink_admin.common.constant;
 
 public class RocketMQConstant {
     public static final String SHORT_LINK_STATES_TOPIC = "short-link-access-stats-topic";
+
+    public static final String SHORT_LINK_STATES_CONSUMER_GROUP = "short-link-access-stats-consumer-group-01";
+
+    public static final String SHORT_LINK_STATES_CHECK_IDEMPOTENT = "short-link-access-stats-check-idempotent";
 }
