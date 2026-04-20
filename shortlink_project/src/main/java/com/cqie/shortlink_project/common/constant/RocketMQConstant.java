8716476d0dab02ea03e355978b6e1d9bc8681a58
Jack kupol <15877767+jack-kupol@user.noreply.gitee.com>
2026-04-20 10:21:18 +0800

    feat(constants): 新增RocketMQ短链缓存一致性常量

---
+++ shortlink_project/src/main/java/com/cqie/shortlink_project/common/constant/RocketMQConstant.java
@@ -0,0 +1,19 @@
+package com.cqie.shortlink_project.common.constant;
+
+public class RocketMQConstant {
+
+    /**
+     * 短链缓存一致性topic
+     */
+    public static final String SHORT_LINK_CACHE_EVICT_TOPIC = "short-link-cache-evict-topic";
+
+    /**
+     * 短链缓存一致性消费者组
+     */
+    public static final String SHORT_LINK_CACHE_EVICT_CONSUMER_GROUP = "short-link-cache-evict-consumer-group01";
+
+    /**
+     * 短链缓存一致性去重检查
+     */
+    public static final String SHORT_LINK_STATES_CHECK_IDEMPOTENT = "short-link-cache-evict-check-idempotent";
+}
