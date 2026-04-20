e4ef6cd69a59823562262dc6b01075e305fe21e0
Jack kupol <15877767+jack-kupol@user.noreply.gitee.com>
2026-04-20 10:21:43 +0800

    feat(util): 添加幂等性检查工具类

---
+++ shortlink_project/src/main/java/com/cqie/shortlink_project/util/IdempotentUtil.java
@@ -0,0 +1,27 @@
+package com.cqie.shortlink_project.util;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
+
+@Component
+@RequiredArgsConstructor
+public class IdempotentUtil {
+
+    private final RedisTemplate<String, String> redisTemplate;
+
+    /**
+     * 检查消息消费是否消费过
+     * @param businessKey 业务键
+     * @param msgId 消息ID
+     * @return true 消息被消费过，false 消息没有被消费过
+     */
+    public boolean checkIdempotent(String businessKey, String msgId) {
+        // 原子 SETNX + 过期时间
+        return !Boolean.TRUE.equals(
+                redisTemplate.opsForValue().setIfAbsent(businessKey + ":" + msgId, "1", 24, TimeUnit.HOURS)
+        );
+    }
+}
