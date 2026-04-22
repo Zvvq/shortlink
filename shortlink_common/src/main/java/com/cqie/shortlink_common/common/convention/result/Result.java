05f10267694334eddf5a9f3664c58b7a981b86d2
Jack kupol <15877767+jack-kupol@user.noreply.gitee.com>
2026-04-22 10:12:07 +0800

    refactor(common): 移动Result类到common模块

--- shortlink_common/src/main/java/com/cqie/shortlink_common/common/convention/result/Result.java
+++ shortlink_common/src/main/java/com/cqie/shortlink_common/common/convention/result/Result.java
@@ -1,9 +1,9 @@
-package com.cqie.shortlink_admin.common.convention.result;
+package com.cqie.shortlink_common.common.convention.result;
 
 
-import com.cqie.shortlink_admin.common.convention.errorcode.BaseErrorCode;
-import com.cqie.shortlink_admin.common.convention.errorcode.IErrorCode;
-import com.cqie.shortlink_admin.common.convention.exception.AbstractException;
+import com.cqie.shortlink_common.common.convention.errorcode.BaseErrorCode;
+import com.cqie.shortlink_common.common.convention.errorcode.IErrorCode;
+import com.cqie.shortlink_common.common.convention.exception.AbstractException;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
