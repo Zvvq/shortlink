62c14686f926c218c66a6cfa8f0dd26a6bab9f2a
Jack kupol <15877767+jack-kupol@user.noreply.gitee.com>
2026-04-22 09:06:53 +0800

    feat(constants): 添加短链缓存一致性死信队列topic及消费者组常量

--- shortlink_project/src/main/java/com/cqie/shortlink_project/common/constant/RocketMQConstant.java
+++ shortlink_project/src/main/java/com/cqie/shortlink_project/common/constant/RocketMQConstant.java
@@ -8,12 +8,24 @@ public class RocketMQConstant {
     public static final String SHORT_LINK_CACHE_EVICT_TOPIC = "short-link-cache-evict-topic";
 
     /**
+     * 短链缓存一致性死信队列topic
+     */
+    public static final String SHORT_LINK_CACHE_EVICT_DEAD_LETTER_TOPIC = "%DLQ%short-link-cache-evict-topic";
+
+    /**
      * 短链缓存一致性消费者组
      */
     public static final String SHORT_LINK_CACHE_EVICT_CONSUMER_GROUP = "short-link-cache-evict-consumer-group01";
 
     /**
+     * 短链缓存一致性死信队列消费者组
+     */
+    public static final String SHORT_LINK_CACHE_EVICT_DEAD_LETTER_CONSUMER_GROUP = "short-link-cache-evict-dead-letter-consumer-group01";
+
+    /**
      * 短链缓存一致性去重检查
      */
     public static final String SHORT_LINK_STATES_CHECK_IDEMPOTENT = "short-link-cache-evict-check-idempotent";
+
+
 }
