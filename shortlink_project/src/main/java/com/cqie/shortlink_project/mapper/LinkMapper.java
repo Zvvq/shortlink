5e0610036afe508d4054e8b690f66921f89bd935
Jack kupol <15877767+jack-kupol@user.noreply.gitee.com>
2026-05-06 11:08:15 +0800

    feat(mapper): 添加按小时汇总的短链接点击量查询方法

--- shortlink_project/src/main/java/com/cqie/shortlink_project/mapper/LinkMapper.java
+++ shortlink_project/src/main/java/com/cqie/shortlink_project/mapper/LinkMapper.java
@@ -1,20 +1,33 @@
 package com.cqie.shortlink_project.mapper;
 
-
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.cqie.shortlink_project.dto.response.LinkClickStatsResponse;
 import com.cqie.shortlink_project.entity.LinkDO;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
+import java.util.List;
 
 /**
-* @author friendA
-* @description 针对表【t_link(短链接表)】的数据库操作Mapper
-* @createDate 2026-04-02 16:24:45
-* @Entity .com.cqie.shortlink_project.entity.TLink
-*/
+ * Mapper for t_link.
+ */
 public interface LinkMapper extends BaseMapper<LinkDO> {
 
+    /**
+     * Sum hourly PV records only. Records with hour = -1 are daily rollups.
+     */
+    @Select({
+            "<script>",
+            "SELECT full_short_url AS linkKey, COALESCE(SUM(pv), 0) AS clickNum ",
+            "FROM t_link_access_stats ",
+            "WHERE del_flag = 0 ",
+            "AND hour BETWEEN 0 AND 23 ",
+            "AND full_short_url IN ",
+            "<foreach collection='linkKeys' item='linkKey' open='(' separator=',' close=')'>",
+            "#{linkKey}",
+            "</foreach> ",
+            "GROUP BY full_short_url",
+            "</script>"
+    })
+    List<LinkClickStatsResponse> selectHourPvStatsByLinkKeys(@Param("linkKeys") List<String> linkKeys);
 }
-
-
-
-
